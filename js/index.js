b843ff0b5dd8ee3ddfbabf4ced5cb5849ebc9f5d
eagleduk <eagleduk@gmail.com>
2022-03-07 22:41:18 +0900

    메뉴 단일화
    메뉴 단일화

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
 const NAMESPACEURI = "http://www.w3.org/2000/svg";
 
+const ADDRESS = [
+  {
+    type: "mail",
+    path: "eagleduk@gmail.com",
+    icon: "/images/email_white_24dp.svg",
+  },
+  {
+    type: "github",
+    path: "https://github.com/eagleduk",
+    icon: "/images/GitHub-Mark-Light-32px.png",
+  },
+];
+
 const SORTLABEL = "sort";
 const SEARCHLABEL = "search";
 const SPPLABEL = "navigate";
@@ -7,28 +20,137 @@ const MSTLABEL = "find";
 
 const RERENDER = "render";
 
-document.querySelectorAll("details").forEach((element) => {
-  element.addEventListener("toggle", (e) => {
-    if (e.target.open) {
-      document.querySelectorAll("details").forEach((detail) => {
-        if (e.target !== detail) detail.open = false;
-      });
-    }
-  });
-  // element.addEventListener("mouseleave", (e) => {
-  //   if (e.target.open) {
-  //     e.target.open = false;
-  //   }
-  // });
-});
+const MODULES = [
+  {
+    key: "sort",
+    title: "sorting",
+    icon: "/images/icons/sort.png",
+    page: "/html/sort.html",
+    modules: [
+      { title: "bubble", value: "bubble" },
+      { title: "selection", value: "selection" },
+      { title: "insertion", value: "insert" },
+    ],
+  },
+  {
+    key: "search",
+    title: "search",
+    icon: "/images/icons/search.png",
+    page: "/html/search.html",
+    modules: [
+      { title: "sequential", value: "sequential" },
+      { title: "binary", value: "binary" },
+      { title: "breadth-first search", value: "bfs" },
+      { title: "depth-first search", value: "dfs" },
+    ],
+  },
+  {
+    key: "spp",
+    title: "shortest path",
+    icon: "/images/icons/icons8-path-30.png",
+    page: "/html/spp.html",
+    modules: [{ title: "dijkstra", value: "dijkstra" }],
+  },
+  {
+    key: "mst",
+    title: "min spanning tree",
+    icon: "/images/icons/neural.png",
+    page: "/html/mst.html",
+    modules: [
+      { title: "kruskal", value: "kruskal" },
+      { title: "prim", value: "prim" },
+    ],
+  },
+];
 
-document.querySelectorAll("div.home-index img").forEach((element) => {
-  const { normalSrc, hoverSrc } = element.dataset;
-  element.addEventListener("mouseover", (e) => {
-    element.src = hoverSrc;
+document.addEventListener("DOMContentLoaded", (e) => {
+  const path = location.pathname;
+  const fullScreenMenu = document.querySelector("div.full-screen-menu");
+  const miniScreenMenu = document.querySelector("div.mini-screen-menu");
+
+  const ul = document.createElement("ul");
+  MODULES.forEach(({ key, title, icon, page, modules }) => {
+    const details = document.createElement("details");
+
+    const summary = document.createElement("summary");
+    const titleSpan = document.createElement("span");
+    titleSpan.title = title;
+    titleSpan.innerText = title;
+    summary.appendChild(titleSpan);
+
+    const ul2 = document.createElement("ul");
+    ul2.classList.add("sub-menu");
+
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    img.src = icon;
+    img.alt = title;
+    img.width = 30;
+
+    li.appendChild(img);
+    modules.forEach(({ title, value }) => {
+      const li2 = document.createElement("li");
+      li2.id = value;
+
+      const anchor = document.createElement("a");
+      anchor.href = `${path === page ? "" : page}#${value}`;
+
+      const titleSpan = document.createElement("span");
+      titleSpan.innerText = title;
+
+      anchor.appendChild(titleSpan);
+
+      li2.appendChild(anchor);
+
+      ul2.appendChild(li2);
+    });
+    details.appendChild(summary);
+    details.appendChild(ul2.cloneNode(true));
+
+    fullScreenMenu.appendChild(details);
+
+    li.appendChild(ul2);
+    ul.appendChild(li);
   });
-  element.addEventListener("mouseout", (e) => {
-    element.src = normalSrc;
+  miniScreenMenu.appendChild(ul);
+
+  const address = document.querySelector("footer address");
+  ADDRESS.forEach(({ type, path, icon }) => {
+    const anchor = document.createElement("a");
+    anchor.href = "#";
+
+    const span = document.createElement("span");
+    span.innerText = path;
+
+    const img = document.createElement("img");
+    img.src = icon;
+    img.alt = type;
+    img.width = 24;
+
+    anchor.appendChild(span);
+    anchor.appendChild(img);
+
+    address.appendChild(anchor);
+  });
+
+  document.querySelectorAll("details").forEach((element) => {
+    element.addEventListener("toggle", (e) => {
+      if (e.target.open) {
+        document.querySelectorAll("details").forEach((detail) => {
+          if (e.target !== detail) detail.open = false;
+        });
+      }
+    });
+  });
+
+  document.querySelectorAll("div.home-index img").forEach((element) => {
+    const { normalSrc, hoverSrc } = element.dataset;
+    element.addEventListener("mouseover", (e) => {
+      element.src = hoverSrc;
+    });
+    element.addEventListener("mouseout", (e) => {
+      element.src = normalSrc;
+    });
   });
 });
 
